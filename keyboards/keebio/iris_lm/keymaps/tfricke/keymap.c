88872761a00aad9c003b4846b472b709bc0ee2a6
Till <till-fricke@gmx.de>
2025-11-03 21:04:16 +0100

    tilde fix

--- keyboards/keebio/iris_lm/keymaps/tfricke/keymap.c
+++ keyboards/keebio/iris_lm/keymaps/tfricke/keymap.c
@@ -15,14 +15,11 @@ enum custom_layers {
 
 enum combos {
     DF_LSFT,
-    JK_LCTL
 };
 
-const uint16_t PROGMEM ctrl_combo[] = {KC_D, KC_F, COMBO_END};
-const uint16_t PROGMEM shft_combo[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM shft_combo[] = {KC_D, KC_F, COMBO_END};
 combo_t key_combos[] = {
-    [DF_LSFT] = COMBO(ctrl_combo, OS_LSFT),
-    [JK_LCTL] = COMBO(shft_combo, OS_LCTL),
+    [DF_LSFT] = COMBO(shft_combo, OS_LSFT),
 };
 
 
@@ -48,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TRNS,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_P0,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TRNS, KC_UNDS, KC_LCBR, KC_LBRC, KC_LPRN,  KC_DEL,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  KC_MINS, KC_UNDS,
+     KC_TRNS, KC_UNDS, KC_LCBR, KC_LBRC, KC_LPRN,  LSFT(KC_GRV),                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  KC_MINS, KC_UNDS,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,  KC_NO,           KC_NO,   KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_MINS, KC_TRNS,
+     KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,  KC_NO,           KC_NO,   KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_BSLS, KC_TRNS,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_TRNS, KC_TRNS, KC_BSPC,                   KC_BSPC, KC_TRNS, KC_TRNS
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -62,9 +59,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TRNS, KC_UNDS, KC_LCBR, KC_LBRC, KC_LPRN,  KC_DEL,                            KC_PIPE, KC_RPRN, KC_RBRC, KC_RCBR, KC_MINS, KC_UNDS,
+     KC_TRNS, KC_UNDS, KC_LCBR, KC_LBRC, KC_LPRN,  LSFT(KC_GRV),                      KC_PIPE, KC_RPRN, KC_RBRC, KC_RCBR, KC_MINS, KC_UNDS,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TRNS, KC_SLSH,  KC_LT,  KC_QUOT, KC_DQUO,  KC_GRV,  KC_NO,            KC_NO,  KC_PLUS, KC_PEQL, KC_COLN,  KC_GT,  KC_BSLS,  KC_TRNS,
+     KC_TRNS, KC_SLSH,  KC_LT,  KC_QUOT, KC_DQUO,  KC_GRV,  KC_NO,            KC_NO,  KC_PLUS, KC_PEQL, KC_COLN,  KC_GT,  KC_UNDS,  KC_TRNS,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
